a757f8e08fedbcc48fb0adf18e3cf9de1afc6153
Bandhu <tapc33@gmail.com>
2024-12-10 09:43:19 +0530

    public updated all

---
+++ app/Http/Controllers/CommentReplyController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CommentReply;
+use Illuminate\Http\Request;
+
+class CommentReplyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CommentReply $commentReply)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CommentReply $commentReply)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, CommentReply $commentReply)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CommentReply $commentReply)
+    {
+        //
+    }
+}
